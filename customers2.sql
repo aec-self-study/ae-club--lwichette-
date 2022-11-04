3229e195e2720efd2fac93401512971081e9c304
lwichette <114984870+lwichette@users.noreply.github.com>
2022-11-04 13:40:07 +0000

    sql style guide

--- customers2.sql
+++ customers2.sql
@@ -1,12 +1,12 @@
 
 with orders as (
-  select
+select
   orders.customer_id as id,
-  MIN(orders.created_at) as first_order_at,
-  MAX(orders.created_at) as last_order_at,
+  min(orders.created_at) as first_order_at,
+  max(orders.created_at) as last_order_at,
   count(orders.id) as number_of_orders
-  FROM `analytics-engineers-club.coffee_shop.orders` AS orders
-  group by 1
+from `analytics-engineers-club.coffee_shop.orders` as orders
+group by 1
 )
 
 select
@@ -16,7 +16,7 @@ select
   orders.first_order_at,
   orders.last_order_at,
   orders.number_of_orders as number_of_orders
-FROM `analytics-engineers-club.coffee_shop.customers` AS customers 
-LEFT JOIN order2 ON customers.id = orders.id
-ORDER BY orders.first_order_at
+from `analytics-engineers-club.coffee_shop.customers` as customers 
+left join order2 on customers.id = orders.id
+order by orders.first_order_at
 limit 5;
