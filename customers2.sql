806e525c048d241733a15feb42a0f8d5372090aa
lwichette <114984870+lwichette@users.noreply.github.com>
2022-11-04 13:03:08 +0000

    created potential conflict

--- customers2.sql
+++ customers2.sql
@@ -1,9 +1,10 @@
-with orders2 as (
+with order2 as (
   select
   orders.customer_id as id,
   MIN(orders.created_at) as first_order_at,
   MAX(orders.created_at) as last_order_at,
   count(orders.id) as number_of_orders
+  --putting a comment here
   FROM `analytics-engineers-club.coffee_shop.orders` AS orders
   group by 1
 )
@@ -12,10 +13,10 @@ select
   customers.id as customer_id,
   customers.name,
   customers.email,
-  orders2.first_order_at,
-  orders2.last_order_at,
-  orders2.number_of_orders as number_of_orders
+  order2.first_order_at,
+  order2.last_order_at,
+  order2.number_of_orders as number_of_orders
 FROM `analytics-engineers-club.coffee_shop.customers` AS customers 
-LEFT JOIN orders2 ON customers.id = orders2.id
-ORDER BY orders2.first_order_at
+LEFT JOIN order2 ON customers.id = order2.id
+ORDER BY order2.first_order_at
 limit 5;
