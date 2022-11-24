b714860a115dd417dc79ec6d5a1f2884616ce7d8
lwichette <114984870+lwichette@users.noreply.github.com>
2022-11-24 14:43:02 +0000

    added exercise on favorite flavors by timestamp

--- advanced_sql/models/all_days.sql
+++ advanced_sql/models/all_days.sql
@@ -1,13 +1,11 @@
-
-
 with generated as (
     {{ dbt_utils.date_spine(
-        datepart="week",
+        datepart="day",
         start_date="cast('2021-01-01' as date)",
-        end_date="cast('2022-10-01' as date)"
+        end_date="cast('2022-12-31' as date)"
     ) }}
 )
 
 select
-    cast(date_week as timestamp) as date_week
+    cast(date_day as timestamp) as date_day
 from generated
