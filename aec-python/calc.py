69d66162cb86fdafb0c30c8f1ab67a6866c0de0e
lwichette <114984870+lwichette@users.noreply.github.com>
2022-12-02 08:50:28 +0000

    added division test and CLI calc.py

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -1,6 +1,22 @@
 import argparse
 
 import argparse
+
+
+def aec_subtract(ints_to_sub):
+    arg_1 = ints_to_sub[0]
+    args_rest = ints_to_sub[1:]
+    our_sub = arg_1 - sum(args_rest)
+    return our_sub
+
+
+def aec_division(args_to_divide):
+    try:
+        return args_to_divide[0]/args_to_divide[1]
+    except:
+         raise ValueError('the division cant be done')
+
+
 parser = argparse.ArgumentParser(
                 description = "CLI calculator"
 )
@@ -20,28 +36,29 @@ div.add_argument('values', type=float, nargs=2)
 mult = subparser.add_parser("multiplicate", help = "multiplicate floats")
 mult.add_argument('values', type=float, nargs='*')
 
-args = parser.parse_args()
 
-if args.command == "add": 
+
+if __name__=="__main__":
+    args = parser.parse_args()
+
+    if args.command == "add": 
         result = sum(args.values)
         print(f"The Sum is: {result}")
     
-elif args.command == "subtract":
-        result = args.values[0]-sum(args.values[1:])
+    elif args.command == "subtract":
+        result = aec_subtract(args.values)
         print(f"The subtraction results in: {result}")
 
-elif args.command == "divide":
-        try:
-            result = args.values[0]/args.values[1]
-            print(f"the division results in: {result}")
-        except:
-            print("the division cant be done")
+    elif args.command == "divide":
+        result = aec_division(args.values)
+        print(f"the division results in: {result}")
+           
 
-elif args.command == "multiplicate":
+    elif args.command == "multiplicate":
             if len(args.values) != 0: 
                 result =  1
                 for n in args.values:
                     result = result * n
-                print(f"the division results in: {result}")
+                print(f"the product results in: {result}")
             else:
                 print(f"empty product leads to: 0")
