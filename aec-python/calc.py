fc471563208e3a02a97b99c9e3f9dfae9473c216
lwichette <114984870+lwichette@users.noreply.github.com>
2022-12-02 09:45:09 +0000

    added format with black

--- aec-python/calc.py
+++ aec-python/calc.py
@@ -14,42 +14,36 @@ def aec_subtract(ints_to_sub):
 
 def aec_division(args_to_divide):
     if len(args_to_divide) != 2:
-        raise ValueError('expected two arguments')
+        raise ValueError("expected two arguments")
     else:
         try:
-            return args_to_divide[0]/args_to_divide[1]
+            return args_to_divide[0] / args_to_divide[1]
         except:
-            raise ValueError('the division cant be done')
+            raise ValueError("the division cant be done")
 
 
-parser = argparse.ArgumentParser(
-                description = "CLI calculator"
-)
+parser = argparse.ArgumentParser(description="CLI calculator")
 
-subparser = parser.add_subparsers(
-                help = "sub-command held",
-                dest = "command" 
-)
+subparser = parser.add_subparsers(help="sub-command held", dest="command")
 
 
-add = subparser.add_parser("add", help = "add integers")
-add.add_argument('values', type=int, nargs='+')
-sub = subparser.add_parser("subtract", help = "subtract integers")
-sub.add_argument('values', type=int, nargs='+')
-div = subparser.add_parser("divide", help = "divide first float by second")
-div.add_argument('values', type=float, nargs=2)
-mult = subparser.add_parser("multiplicate", help = "multiplicate floats")
-mult.add_argument('values', type=float, nargs='*')
+add = subparser.add_parser("add", help="add integers")
+add.add_argument("values", type=int, nargs="+")
+sub = subparser.add_parser("subtract", help="subtract integers")
+sub.add_argument("values", type=int, nargs="+")
+div = subparser.add_parser("divide", help="divide first float by second")
+div.add_argument("values", type=float, nargs=2)
+mult = subparser.add_parser("multiplicate", help="multiplicate floats")
+mult.add_argument("values", type=float, nargs="*")
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     args = parser.parse_args()
 
-    if args.command == "add": 
+    if args.command == "add":
         result = sum(args.values)
         print(f"The Sum is: {result}")
-    
+
     elif args.command == "subtract":
         result = aec_subtract(args.values)
         print(f"The subtraction results in: {result}")
@@ -57,13 +51,12 @@ if __name__=="__main__":
     elif args.command == "divide":
         result = aec_division(args.values)
         print(f"the division results in: {result}")
-           
 
     elif args.command == "multiplicate":
-            if len(args.values) != 0: 
-                result =  1
-                for n in args.values:
-                    result = result * n
-                print(f"the product results in: {result}")
-            else:
-                print(f"empty product leads to: 0")
+        if len(args.values) != 0:
+            result = 1
+            for n in args.values:
+                result = result * n
+            print(f"the product results in: {result}")
+        else:
+            print(f"empty product leads to: 0")
--- aec-python/tests/test_divide.py
+++ aec-python/tests/test_divide.py
@@ -1,22 +1,24 @@
 import unittest
 from calc import aec_division
 
+
 class Test_Divide(unittest.TestCase):
     def test_divide_ordinary(self):
         enum = 1
 
         result1 = aec_division([enum, 2])
         self.assertEqual(result1, 0.5)
-    
+
     def test_divide_zero_denom(self):
         enum = 1
         with self.assertRaises(Exception):
             aec_division([enum, 0])
-    
+
     def test_divide_number_args(self):
         args = [1, 2, 3]
         with self.assertRaises(ValueError):
             aec_division(args)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
